b81ea20d3aa6e61de3c6ec2d3ee401e23e293469
Abhijit Gupta <avgupta456@gmail.com>
2022-04-09 00:58:36 -0400

    add starter sqlmodel code

---
+++ backend/db/main.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from sqlmodel import Field, SQLModel, create_engine, Session
+
+from db.models import *
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
