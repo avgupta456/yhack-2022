ae8ef6e12eb0ecd13dc22302398cd33a1155ba69
jadechen <57021425+jadechen@users.noreply.github.com>
2022-04-09 17:30:57 -0400

    list of assignments with hours

--- simulation/test.py
+++ simulation/test.py
@@ -12,7 +12,7 @@ class Assignment:
         self.hours_left = hours_left
 
 t1 = Assignment("181 pset", "physics pset", 0, 7, 3, 3)
-t2 = Assignment("quiz", "physics quiz", 5, 7, 0.1, 0.1)
+t2 = Assignment("quiz", "physics quiz", 5, 7, 1, 1)
 t3 = Assignment("lab report", "fluids lab report", 2, 9, 15, 15)
 t4 = Assignment("389 pset", "heat transfer pset", 4, 11, 2, 2)
 t5 = Assignment("research", "research", 0, 13, 16, 16)
@@ -24,20 +24,32 @@ times = [6, 3, 3, 3, 3, 4, 6, 6, 3, 3, 3, 3, 4, 6]
 
 #prints the task that has the nearest due date with most amount of time left
 def todo(tasks):
-    do_next = t1
-    for i in tasks:
+    for x in tasks:
+        if x.hours_left <= 0:
+            tasks.remove(x)
+    if len(tasks) == 0:
+        return None
+    do_next = tasks[0]
+    for i in tasks: 
         if i.end_date < do_next.end_date:
             do_next = i
         elif i.end_date == do_next.end_date and i.hours_left > do_next.hours_left:
             do_next = i
     return do_next
 
-todo(tasks)
-
+#
 def schedule(tasks, times):
     numHours = 0
     for i in times:
         numHours = numHours + i
     print(numHours)
+    
+    for j in range(numHours):
+        do_next = todo(tasks)
+        if do_next is None:
+            print(j, "Free Time")
+        else:
+            do_next.hours_left -= 1
+            print(j, do_next.name)
 
 schedule(tasks, times)
