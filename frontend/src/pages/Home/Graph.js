eb259b44603783b343bafb34c078439dc8c7a0d3
Abhijit Gupta <avgupta456@gmail.com>
2022-04-10 00:57:51 -0400

    Finish Graph

--- frontend/src/pages/Home/Graph.js
+++ frontend/src/pages/Home/Graph.js
@@ -1,12 +1,8 @@
 /* eslint-disable */
 
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
-import {
-  GraphView,
-  BwdlTransformer, // optional, Example JSON transformer
-  GraphUtils, // optional, useful utility functions
-} from "react-digraph";
+import { GraphView } from "react-digraph";
 
 const GraphConfig = {
   NodeTypes: {
@@ -65,7 +61,16 @@ const days = [
   "Thursday",
   "Friday",
   "Saturday",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
+
+const N = days.length;
 
 const sample = {
   nodes: [
@@ -77,26 +82,12 @@ const sample = {
       type: "startDate",
     })),
     ...days.map((day, i) => ({
-      id: i + days.length,
+      id: i + N,
       title: day,
       x: 175 * i + 100,
       y: 700,
       type: "endDate",
     })),
-    {
-      id: 14,
-      title: "Node A",
-      x: 200,
-      y: 300,
-      type: "task",
-    },
-    {
-      id: 15,
-      title: "Node B",
-      x: 500,
-      y: 200,
-      type: "task",
-    },
   ],
   edges: [],
 };
@@ -106,12 +97,78 @@ const Graph = () => {
   const [edges, setEdges] = useState(sample.edges);
   const [selected, setSelected] = useState(null);
 
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [hoursDone, setHoursDone] = useState(0);
+  const [hoursLeft, setHoursLeft] = useState(2);
+  const [showModal, setShowModal] = useState(-1);
+
+  const setModal = (id) => {
+    setShowModal(id);
+    setName(nodes[id].title);
+    setDescription(nodes[id].description);
+    setHoursDone(nodes[id].hoursDone);
+    setHoursLeft(nodes[id].hoursLeft);
+  };
+
+  const updateNode = () => {
+    const newNodes = nodes.map((node) => {
+      if (node.id === showModal) {
+        return {
+          ...node,
+          title: name,
+          description: description,
+          hoursDone: hoursDone,
+          hoursLeft: hoursLeft,
+        };
+      }
+      return node;
+    });
+    setNodes(newNodes);
+    setShowModal(-1);
+    setSelected(null);
+  };
+
+  const createNode = () => {
+    const id = Math.max(...nodes.map((node) => node.id)) + 1;
+    const newNodes = [
+      ...nodes,
+      {
+        id,
+        x: 400,
+        y: 400,
+        title: name,
+        description: description,
+        hoursDone: hoursDone,
+        hoursLeft: hoursLeft,
+        type: "task",
+      },
+    ];
+    const newEdges = [
+      ...edges,
+      { source: 0, target: id, type: "edge" },
+      { source: id, target: 2 * N - 1, type: "edge" },
+    ];
+    setNodes(newNodes);
+    setEdges(newEdges);
+    setModal(id);
+  };
+
   const NodeTypes = GraphConfig.NodeTypes;
   const NodeSubtypes = GraphConfig.NodeSubtypes;
   const EdgeTypes = GraphConfig.EdgeTypes;
 
   const onSelect = (selected) => {
+    console.log(selected);
     setSelected(selected);
+    if (selected.nodes?.length == 0) {
+      return;
+    }
+    const node = [...selected.nodes][0][1];
+    if (node.type !== "task") {
+      return;
+    }
+    setModal(node.id);
   };
 
   const setStartDate = (node, newEdge) => {
@@ -158,10 +215,13 @@ const Graph = () => {
   };
 
   const onCreateNode = (x, y) => {
-    const id = nodes.length;
+    const id = Math.max(...nodes.map((node) => node.id)) + 1;
     const newNode = {
       id: id,
       title: "New Task",
+      description: "",
+      hoursDone: 0,
+      hoursLeft: 2,
       x,
       y,
       type: "task",
@@ -170,14 +230,23 @@ const Graph = () => {
     const newEdges = [
       ...edges,
       { source: 0, target: id, type: "edge" },
-      { source: id, target: 13, type: "edge" },
+      { source: id, target: 2 * N - 1, type: "edge" },
     ];
     setNodes(newNodes);
     setEdges(newEdges);
+    setModal(id);
   };
 
   const onDeleteSelected = (selected) => {
-    if (selected.nodes.length == 0) {
+    console.log(selected);
+    if (!selected.nodes || selected.nodes.length === 0) {
+      console.log("HERE");
+      const edge = [...selected.edges][0][1];
+      console.log(edge);
+      const newEdges = edges.filter((e) => {
+        return !(e.source === edge.source && e.target === edge.target);
+      });
+      setEdges([...newEdges]);
       return;
     }
     const node = [...selected.nodes][0][1];
@@ -191,43 +260,155 @@ const Graph = () => {
     const newNodes = nodes.filter((n) => n[NODE_KEY] !== node.id);
     setNodes([...newNodes]);
     setSelected(null);
+    setShowModal(-1);
   };
 
   return (
-    <div className="w-full h-full border-1" id="graph">
-      <GraphView
-        ref={useRef("GraphView")}
-        gridSpacing={175}
-        layoutEngineType={"SnapToGrid"}
-        allowMultiselect={false}
-        showGraphControls={true}
-        zoomDur={0}
-        renderNodeText={(node) => (
-          <text x={-6.5 * node.title.length} y={5} fontSize={"24px"}>
-            {node.title}
-          </text>
+    <div className="flex h-full w-full">
+      <div className="w-1/3 h-full p-8">
+        {showModal > 0 ? (
+          <div>
+            <p className="mb-1 text-2xl text-gray-700">Edit Task</p>
+            <hr />
+            <div className="my-6">
+              <input type="hidden" name="remember" value="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div className="my-2">
+                  Task Title
+                  <input
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="my-2">
+                  Task Description
+                  <input
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+                <div className="flex">
+                  <div className="w-1/2 mt-2 mr-2">
+                    Hours Done
+                    <input
+                      required
+                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                      value={hoursDone}
+                      onChange={(e) => setHoursDone(e.target.value)}
+                    />
+                  </div>
+                  <div className="w-1/2 mt-2 ml-2">
+                    Hours Left
+                    <input
+                      required
+                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                      value={hoursLeft}
+                      onChange={(e) => setHoursLeft(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="my-6">
+                <button
+                  type="button"
+                  onClick={updateNode}
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Edit Task
+                </button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <p className="mb-1 text-2xl text-gray-700">Create Task</p>
+            <hr />
+            <div className="my-6">
+              <input type="hidden" name="remember" value="true" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div className="my-2">
+                  Task Title
+                  <input
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder=""
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="my-2">
+                  Task Description
+                  <input
+                    required
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                    placeholder=""
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+                <div className="flex">
+                  <div className="w-1/2 mt-2 mr-2">
+                    Hours Done
+                    <input
+                      required
+                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                      placeholder=""
+                      onChange={(e) => setHoursDone(e.target.value)}
+                    />
+                  </div>
+                  <div className="w-1/2 mt-2 ml-2">
+                    Hours Left
+                    <input
+                      required
+                      className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                      placeholder=""
+                      onChange={(e) => setHoursLeft(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="my-6">
+                <button
+                  type="button"
+                  onClick={createNode}
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                >
+                  Create Task
+                </button>
+              </div>
+            </div>
+          </div>
         )}
-        nodeKey={NODE_KEY}
-        nodes={nodes}
-        edges={edges}
-        selected={selected}
-        nodeTypes={NodeTypes}
-        nodeSubtypes={NodeSubtypes}
-        edgeTypes={EdgeTypes}
-        onSelect={onSelect}
-        onCreateEdge={onCreateEdge}
-        onCreateNode={onCreateNode}
-        onDeleteSelected={onDeleteSelected}
-        /*
-        onSelect={this.onSelect}
-        onCreateNode={this.onCreateNode}
-        onUpdateNode={this.onUpdateNode}
-        onDeleteNode={this.onDeleteNode}
-        onCreateEdge={this.onCreateEdge}
-        onSwapEdge={this.onSwapEdge}
-        onDeleteEdge={this.onDeleteEdge}
-        */
-      />
+      </div>
+      <div className="w-2/3 h-full border-1" id="graph">
+        <GraphView
+          ref={useRef("GraphView")}
+          gridSpacing={175}
+          layoutEngineType={"SnapToGrid"}
+          allowMultiselect={false}
+          showGraphControls={true}
+          zoomDur={0}
+          renderNodeText={(node) => (
+            <text x={-6.5 * node.title.length} y={5} fontSize={"24px"}>
+              {node.title}
+            </text>
+          )}
+          nodeKey={NODE_KEY}
+          nodes={nodes}
+          edges={edges}
+          selected={selected}
+          nodeTypes={NodeTypes}
+          nodeSubtypes={NodeSubtypes}
+          edgeTypes={EdgeTypes}
+          onSelect={onSelect}
+          onCreateEdge={onCreateEdge}
+          onCreateNode={onCreateNode}
+          onDeleteSelected={onDeleteSelected}
+          onBackgroundClick={() => setShowModal(-1)}
+        />
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -4,10 +4,7 @@ import Graph from "./Graph";
 const HomeScreen = () => {
   return (
     <div className="w-full h-full flex">
-      <div className="w-1/3 bg-red-100">Test</div>
-      <div className="w-2/3 bg-blue-100">
-        <Graph />
-      </div>
+      <Graph />
     </div>
   );
 };
